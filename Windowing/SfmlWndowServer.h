67507bd85f00323adbc75bdcd7c8b51c66b67b0e
Dean Tuckey <dean243@hotmail.com>
2021-07-09 13:57:00 +1000

    rename pt1

--- Windowing/SfmlWndowServer.h
+++ Windowing/SfmlWndowServer.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <unordered_map>
+#include "../Gfx/SfmlBoundObj.h"
 #include "WindowServerAPI.h"
 
 namespace Amendieres::Windowing
 {
     class SfmlWindowServer : public WindowServerAPI
     {
+    public:
         void Init(const std::string& configName) override;
         void Shutdown() override;
         bool ReloadConfig(const std::string& newConfig = nullptr) override;
@@ -15,5 +18,8 @@ namespace Amendieres::Windowing
         void ExtWindow_Destroy(ExtWindow* window) override;
         bool ExtWindow_Resize(ExtWindow* window, const uint64_t newWidth, const uint64_t newHeight) override;
         void ExtWindow_GoFullscreen(ExtWindow* window) override;
+    
+    private:
+        std::unordered_map<ExtWindow*, Gfx::SfmlBoundObj> windows;
     };
 }
