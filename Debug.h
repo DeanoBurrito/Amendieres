5bc83b2bcc49e5ff45960d96ee121bd5b819fe83
Dean T <dean243@hotmail.com>
2021-07-09 10:47:20 +1000

    Added LOG() macro to debug.h

--- Debug.h
+++ Debug.h
@@ -8,6 +8,8 @@
 #ifdef DEBUG_LOGGING
     #include <iostream>
     #define LOG_ERROR(x) std::cerr << "[ERROR] " << #x << std::endl;
+    #define LOG(x) std::cout << "[DEBUG] " << #x << std::endl;
 #else
     #define LOG_ERROR(X)
+    #define LOG(x)
 #endif
