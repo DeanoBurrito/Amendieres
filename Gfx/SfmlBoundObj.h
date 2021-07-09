5c4b50ca0ef39b722fb1e7aa282aa81a30f8a386
Dean T <dean243@hotmail.com>
2021-07-09 11:51:11 +1000

    Added SFML bound obj wrapper

---
+++ Gfx/SfmlBoundObj.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace Amendieres::Gfx
+{
+    enum class SfmlBoundObjType
+    {};
+
+    class SfmlBoundObj
+    {
+    public:
+        const SfmlBoundObjType type;
+        void* const localObj = nullptr;
+        void* const sfObj = nullptr;
+
+        SfmlBoundObj() = delete;
+        SfmlBoundObj(void* const local, void* const sfml, SfmlBoundObjType linkType)
+        : type(linkType), localObj(local), sfObj(sfml)
+        {}
+    };
+}
