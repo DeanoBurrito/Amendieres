74c6f0af22301c0fc37ac938e21bfc692b8b5b9f
Dean T <dean243@hotmail.com>
2021-07-09 11:52:05 +1000

    Added WindowServerAPI class

---
+++ Gfx/WindowServerAPI.cpp
@@ -0,0 +1,26 @@
+#include "../Debug.h"
+#include "WindowServerAPI.h"
+
+namespace Amendieres::Gfx
+{
+    static WindowServerAPI* currentInst = nullptr;
+
+    void WindowServerAPI::SetInstance(WindowServerAPI *const inst)
+    {
+        if (currentInst != nullptr)
+        {
+            LOG_ERROR("WindowSever instance is being overwritten by another server, this may have unintended consequences!")
+        }
+        currentInst = inst;
+    }
+
+
+    WindowServerAPI* WindowServerAPI::The()
+    {
+        if (currentInst == nullptr)
+        {
+            LOG_ERROR("Get() on WindowServerAPI instance is currently nullptr!");
+        }
+        return currentInst;
+    }
+}
